e4fd4a5fddb99e845c97d98a5251949733f25aa9
UDIT AGRAWAL <udiwal789@hotmail.com>
2017-06-02 02:34:29 +0530

    Initial commit
    New Files Added

---
+++ ToDoList/todo.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="todo.aspx.cs" Inherits="ToDoList.todo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta name="description" content="author:udit agrawal" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" />   
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <title>Tasks</title>
+    <style>
+        body {
+    background-color : whitesmoke;
+}
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div><br /><br />
+        <div class="container">
+     <br /><br />
+            <div class ="row">
+                <div class="col-md-offset-4 col-md-2"><asp:TextBox ID="txtTask" runat="server" placeholder="Enter Task"></asp:TextBox></div>&nbsp;&nbsp;
+                <div class="col-md-2"><asp:Button ID="btnAdd" runat="server" Text="Add Task" CssClass="btn btn-primary" Enabled="true" OnClick="btnAdd_Click" /></div>
+            </div><br /><br />
+            <div class="row">
+                <div class="col-md-offset-4 col-md-1"><asp:Button ID="btnView" runat="server" Text="View Tasks" CssClass="btn btn-info" Enabled="true" OnClick="btnView_Click" /></div>
+                &nbsp;
+                <div class="col-md-2"><asp:Button ID="btnViewComplete" runat="server" Text="View Completed tasks" CssClass="btn btn-info" Enabled="true" OnClick="btnViewComplete_Click" /></div>
+                </div>
+                </div>
+            <br /><br />
+
+            <div class="row">
+                <div class="col-md-offset-3 col-md-6 text-center">
+                    <table class="table table-condensed table-bordered table-responsive lead">
+                        <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+                    </table>
+                    </div>
+            </div>
+
+            <div class="row">
+                 <div class="col-md-offset-5 col-md-6">
+                    <asp:CheckBoxList ID="CheckBoxList1" runat="server" Visible="false"></asp:CheckBoxList>                         
+                            <asp:Button ID="btnModify" runat="server" Text="Modify Tasks" CssClass="btn btn-primary" Visible="false" OnClick="btnModify_Click" />
+                </div>
+                </div>
+    </div>
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ToDoList/todo.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace ToDoList
+{
+    public partial class todo : System.Web.UI.Page
+    {
+        public class Task
+        {
+            public string taskName;
+            public bool completed;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            string json;
+            Task t = new Task();
+            t.taskName = txtTask.Text.Trim();
+            t.completed = false;
+            try
+            {
+                using (StreamReader sr = new StreamReader(Server.MapPath("~/") + @"taskList.json"))
+                {
+                    json = sr.ReadToEnd();
+                    List<Task> tasks = JsonConvert.DeserializeObject<List<Task>>(json);
+                    if (tasks == null)
+                        json = JsonConvert.SerializeObject(t, Formatting.Indented);
+                    else
+                    {
+                        tasks.Add(t);
+                        json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+                    }
+                }
+
+                File.WriteAllText(Server.MapPath("~/") + @"taskList.json", json);
+                Response.Write("<script>alert('Task Added Succesfully');</script>");
+            }
+  
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert(" + ex.Message + "Error in adding task');</script>");
+            }
+
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CheckBoxList1.Visible = true;   
+                CheckBoxList1.Items.Clear();
+                btnModify.Visible = true;
+                using (StreamReader sr = new StreamReader(Server.MapPath("~/") + @"taskList.json"))
+                {
+                    List<ListItem> ck = new List<ListItem>();
+                    //ck.ID = "check1";
+                    string json = sr.ReadToEnd();
+                    dynamic items = JsonConvert.DeserializeObject(json);
+                    foreach(var item in items)
+                    {
+                        ListItem c = new ListItem();
+                        c.Text = item.taskName;
+                        c.Value = item.completed;
+                        if (item.completed == false)
+                            c.Selected = false;
+                        else
+                            c.Selected = true;
+                        CheckBoxList1.Items.Add(c);
+                    }
+                    //CheckBoxList1.DataSource = ck;
+                    //CheckBoxList1.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert(" + ex.Message + "Error in viewing tasks');</script>");
+            }
+        }
+
+        protected void btnViewComplete_Click(object sender, EventArgs e)
+        {
+            CheckBoxList1.Visible = false;
+            btnModify.Visible = false;
+            using (StreamReader sr = new StreamReader(Server.MapPath("~/") + @"taskList.json"))
+            {
+                string json = sr.ReadToEnd();
+                dynamic items = JsonConvert.DeserializeObject(json);
+                StringBuilder html = new StringBuilder();
+                html.Append("<thead class='text-danger'>");
+                html.Append("<tr>");
+                html.Append("<th class='text-center'>"); html.Append("Task Name"); html.Append("</th>");
+                html.Append("<th class='text-center'>"); html.Append("Completed"); html.Append("</th>");
+                html.Append("</tr>");
+                html.Append("</thead>");
+                html.Append("<tbody>");
+                foreach (var item in items)
+                {
+                    html.Append("<tr>");
+                    html.Append("<td>"); html.Append(item.taskName); html.Append("</td>");
+                    if (item.completed == false)
+                    { html.Append("<td class='text text-danger'>"); html.Append("Not Completed"); html.Append("</td>"); }
+                    else
+                    { html.Append("<td class='text text-info'>"); html.Append("Completed"); html.Append("</td>"); }
+                        html.Append("</tr>");
+                }
+                html.Append("</tbody>");
+                PlaceHolder1.Controls.Clear();
+                PlaceHolder1.Controls.Add(new Literal { Text = html.ToString() });
+            }
+        }
+
+        protected void btnModify_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CheckBoxList1.Visible = true;
+                List<Task> tasks = new List<Task>();
+                foreach (ListItem item in CheckBoxList1.Items)
+                {
+                    Task t = new Task();
+                    t.taskName = item.Text;
+                    t.completed = item.Selected;
+                    tasks.Add(t);
+                }
+                string json = JsonConvert.SerializeObject(tasks, Formatting.Indented);
+                File.WriteAllText(Server.MapPath("~/") + @"taskList.json", json);
+                Response.Write("<script>alert('Tasks Modified Succesfully');</script>");
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert(" + ex.Message + "Error in modifying tasks information');</script>");
+            }
+        }
+    }
+}
